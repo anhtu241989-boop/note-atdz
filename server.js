86b949a0b94848b2d2aefd951125ec0a3c419a23
anhtu241989-boop <anhtu241989@gmail.com>
2025-09-21 17:27:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// EJS template
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// Static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Gắn routes
+// Routes
 app.use("/", notesRoutes);
 
 // 404 fallback
